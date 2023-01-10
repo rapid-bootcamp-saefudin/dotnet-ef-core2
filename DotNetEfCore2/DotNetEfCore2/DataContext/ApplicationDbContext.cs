d743765b2e94a6b67b840596cc6c2394f7e210e9
Muhammad Saefudin <itsapud@gmail.com>
2023-01-10 09:19:14 +0700

    Day 7 - Create New DotNetEFCore Project

---
+++ DotNetEfCore2/DotNetEfCore2/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DotNetEfCore2.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<CategoryEntity> CategoryEntities => Set<CategoryEntity>();
+    }
+}
---
+++ DotNetEfCore2/DotNetEfCore2/DataContext/CategoryEntity.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotNetEfCore2.DataContext
+{
+    [Table("tbl_category")]
+    public class CategoryEntity
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("category_code")]
+        public String CategoryCode { get; set; }
+
+        [Column("category_name")]
+        public String CategoryName { get; set; }
+
+        [Column("category_description")]
+        public String CategoryDescription { get; set; }
+
+        public CategoryEntity() { }
+    }
+
+}
